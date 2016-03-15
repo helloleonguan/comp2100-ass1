14c22736b70cd4fcc6974af1e048892eb33adf48
u5684206 <u5684206@anu.edu.au>
2016-03-16 00:25:49 +1100

    Completed Milestone 1

--- Notes/app/src/main/java/com/example/administrator/notes/EditActivity.java
+++ Notes/app/src/main/java/com/example/administrator/notes/EditActivity.java
@@ -23,7 +23,6 @@ public class EditActivity extends AppCompatActivity {
 
     EditText etHeading;
     EditText etContent;
-    Button saveEditButton;
     Button deleteEditButton;
 
     TextView tvHeading;
@@ -46,13 +45,14 @@ public class EditActivity extends AppCompatActivity {
 
         etHeading = (EditText) findViewById(R.id.EditTextForHeading);
         etContent = (EditText) findViewById(R.id.EditTextForMainContent);
-        saveEditButton = (Button) findViewById(R.id.saveEditBtn);
         deleteEditButton = (Button) findViewById(R.id.deleteEditBtn);
 
         tvHeading = (TextView) findViewById(R.id.TextViewForHeading);
         tvContent = (TextView) findViewById(R.id.TextViewForContent);
         editViewButton = (Button) findViewById(R.id.editViewBtn);
         deleteViewButton = (Button) findViewById(R.id.deleteViewBtn);
+
+        SharedPreferences data = getSharedPreferences(MainActivity.FILE_NAME, MODE_PRIVATE);
 
         if (mode == 0) {
             //Editing mode
@@ -65,6 +65,8 @@ public class EditActivity extends AppCompatActivity {
             //Viewing mode
             edit.setVisibility(View.GONE);
             view.setVisibility(View.VISIBLE);
+            tvHeading.setText(data.getString(noteID + "heading",""));
+            tvContent.setText(data.getString(noteID + "content", ""));
         } else {
             System.out.println("Error mode data!");
         }
@@ -72,22 +74,27 @@ public class EditActivity extends AppCompatActivity {
     }
 
     public void saveNote(View v) {
-        //Check whether the content&heading are both empty. TODO
-        SharedPreferences data = getSharedPreferences(MainActivity.FILE_NAME, MODE_PRIVATE);
-        SharedPreferences.Editor editor = data.edit();
-        editor.putString(noteID + "heading",etHeading.getText().toString());
-        editor.putString(noteID + "content", etContent.getText().toString());
-        editor.commit(); //Using the returned boolean value + if statement to determine 2 possible outcomes TODO
+        if (etHeading.getText().toString().equals("") && etContent.getText().toString().equals("") && edit.getVisibility() == View.VISIBLE) {
+            Toast.makeText(this, "Note has not been saved because it is empty.", Toast.LENGTH_SHORT).show();
+        } else if (view.getVisibility() == View.VISIBLE) {
+            return;
+        } else {
+            SharedPreferences data = getSharedPreferences(MainActivity.FILE_NAME, MODE_PRIVATE);
+            SharedPreferences.Editor editor = data.edit();
+            editor.putString(noteID + "heading", etHeading.getText().toString());
+            editor.putString(noteID + "content", etContent.getText().toString());
+            editor.commit(); //Using the returned boolean value + if statement to determine 2 possible outcomes TODO
 
-        etHeading.setText("");
-        etContent.setText("");
+            etHeading.setText("");
+            etContent.setText("");
 
-        Toast.makeText(this, "Note has been saved!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Note has been saved!", Toast.LENGTH_SHORT).show();
 
-        edit.setVisibility(View.GONE);
-        view.setVisibility(View.VISIBLE);
-        tvHeading.setText(data.getString(noteID + "heading", "No such note!"));
-        tvContent.setText(data.getString(noteID + "content", "No such note!"));
+            edit.setVisibility(View.GONE);
+            view.setVisibility(View.VISIBLE);
+            tvHeading.setText(data.getString(noteID + "heading", "No such note!"));
+            tvContent.setText(data.getString(noteID + "content", "No such note!"));
+        }
 
     }
 
@@ -109,32 +116,18 @@ public class EditActivity extends AppCompatActivity {
         editor.remove(noteID + "content");
         editor.commit(); //Using the returned boolean value + if statement to determine 2 possible outcomes TODO
 
-        finish();
+        edit.setVisibility(View.VISIBLE);
+        view.setVisibility(View.GONE);
+        etHeading.setText("");
+        etContent.setText("");
+
         Toast.makeText(this, "Note has been deleted!", Toast.LENGTH_SHORT).show();
     }
 
-
-// Using backPressed TODO
-//        if (edit.getVisibility() == View.VISIBLE){
-//            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-//            alertDialogBuilder.setMessage("You haven't save the note. Do you want to save it?");
-//
-//            alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    saveNote(edit);
-//                }
-//            });
-//            alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    deleteNote(edit);
-//                }
-//            });
-//
-//            AlertDialog alertDialog = alertDialogBuilder.create();
-//            alertDialog.show();
-//        } else {
-//       }
+    @Override
+    public void onPause() {
+        saveNote(edit);
+        super.onPause();
+    }
 
 }
--- Notes/app/src/main/java/com/example/administrator/notes/MainActivity.java
+++ Notes/app/src/main/java/com/example/administrator/notes/MainActivity.java
@@ -7,26 +7,65 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     public static final String FILE_NAME = "MyNotes";
-    public int serial_number = 0;
+    public int serial_number;
+
+    GridView gridview;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gridview = (GridView) findViewById(R.id.notesGrid);
+
+        SharedPreferences data = getSharedPreferences(MainActivity.FILE_NAME, MODE_PRIVATE);
+        serial_number = data.getInt("serial_number",0);
+
 //        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 //        SharedPreferences.Editor editor = settings.edit();
 
+
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        //Update the note list TODO
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences data = getSharedPreferences(MainActivity.FILE_NAME, MODE_PRIVATE);
+        serial_number = data.getInt("serial_number",0);
+
+        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+        for(int i = 0; i <= serial_number; i++){
+            if (data.contains(i + "heading") && data.contains(i + "content")) {
+                Map<String, Object> listItem = new HashMap<>();
+                listItem.put("heading", data.getString(i + "heading", ""));
+                listItem.put("content", data.getString(i + "content", ""));
+                listItem.put("ID", i);
+                listItems.add(listItem);
+            }
+        }
+
+        gridview.setAdapter(new SimpleAdapter(this,listItems,R.layout.note_item,new String[]{"heading", "content","ID"},
+                new int[]{R.id.itemHeading,R.id.itemContent,R.id.itemID}));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Map<String, Object> obj = ( Map<String, Object>) gridview.getAdapter().getItem(position);
+                openNote(gridview, obj.get("ID").toString());
+            }
+        });
     }
 
     public void newNote (View v) {
@@ -37,9 +76,22 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openNote (View v) {
-
+    public void openNote (View v, String noteID) {
+        Intent intent = new Intent(this,EditActivity.class);
+        intent.putExtra("noteID",noteID);
+        intent.putExtra("mode",1); //  stands for viewing mode.
+        startActivity(intent);
     }
 
     //Adapater to content new notes TODO
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        SharedPreferences data = getSharedPreferences(MainActivity.FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = data.edit();
+        editor.putInt("serial_number", serial_number);
+        editor.commit();
+    }
 }
